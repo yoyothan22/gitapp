aab6a301569103dbc17453e48e9fe52bfa3414af
yoyothan22 <107935550+yoyothan22@users.noreply.github.com>
2023-01-14 07:54:29 -0800

    Add files via upload
    latest 14.01.2023 17:55

--- gitapp.py
+++ gitapp.py
@@ -1,8 +1,13 @@
 import os
 URL = input("please input a git url")
-PATH_TO_CLONE = input("where to clone?")
-CLONE = os.system("git clone"+ " "+URL+ " " +PATH_TO_CLONE)
+# PATH_TO_CLONE = input("where to clone?")
+if os.path.exists('/home/yonathane/project/gitapp/clone'):
+    os.system("rm -r /home/yonathane/project/gitapp/clone/*")
+    os.system("sudo rm -r /home/yonathane/project/gitapp/clone/.git")
+
+CLONE = os.system("git clone"+ " "+URL+ " " + "~/project/gitapp/clone")
 print ("finding local branches ...")
 SHOW_BRANCH = os.system("git branch")
 REPO_TO_CLONE = input("please provide the branch")
 # BRANCH_TO_MERGE = input()
+# git merge source-branch && git branch -d source-branch
