1c520b0bfe02269573e0878b3589593df618b0fe
yoyothan22 <107935550+yoyothan22@users.noreply.github.com>
2023-01-15 11:16:19 -0800

    15.01.2023

--- gitapp.py
+++ gitapp.py
@@ -1,13 +1,12 @@
 import os
-URL = input("please input a git url")
+URL = input("please input a git url : ")
 # PATH_TO_CLONE = input("where to clone?")
 if os.path.exists('/home/yonathane/project/gitapp/clone'):
     os.system("rm -r /home/yonathane/project/gitapp/clone/*")
     os.system("sudo rm -r /home/yonathane/project/gitapp/clone/.git")
-
+errormsg =  "error"
 CLONE = os.system("git clone"+ " "+URL+ " " + "~/project/gitapp/clone")
-print ("finding local branches ...")
+print ("Finding Local Branches ...")
 SHOW_BRANCH = os.system("git branch")
-REPO_TO_CLONE = input("please provide the branch")
-# BRANCH_TO_MERGE = input()
-# git merge source-branch && git branch -d source-branch
+REPO_TO_CLONE = input("please provide the Branch : ")
+os.system("git merge"+" "+ REPO_TO_CLONE + "2>  ~/project/log.txt" ) 
